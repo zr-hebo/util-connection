bea9f2a16becea23fe184638da9c748a1398d10d
河伯 <zrhebo@gmail.com>
2017-08-01 14:50:42 +0800

    add user define close connection func

---
+++ conn_operation.go
@@ -0,0 +1,22 @@
+package connassist
+
+import (
+	"database/sql"
+
+	"github.com/zssky/log"
+)
+
+// CloseConnection 关闭数据库连接
+func CloseConnection(conn *sql.DB) (err error) {
+	err = conn.Close()
+
+	connInfo, queryErr := GetMySQLConnInfo(conn)
+	if queryErr != nil {
+		log.Errorf("获取连接描述信息失败 <-- %s", queryErr.Error())
+
+	} else {
+		log.Infof("关闭连接后，连接描述信息：%s", connInfo)
+	}
+
+	return
+}
